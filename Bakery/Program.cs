a31ba7bc23429ead893478380cb92b3121f7247d
Morgan Bradford <morganjbradford95@gmail.com>
2021-02-26 18:37:41 -0600

    adjust welcome message

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -8,7 +8,9 @@ namespace Bakery
     public static void Main()
     {
       Console.WriteLine("----------Pierre's----------");
-      Console.WriteLine("Welcome to Pierre's Bakery, we've the finest selection of bread and pastries. What kind? What do you mean what kind. Bread and pastries, those kinds!");
+      Console.WriteLine("Welcome to Pierre's Bakery, we've the finest selection of bread and pastries.");
+      Console.WriteLine("Bread: $5/each. We're also running a sale for buy 2 get 1 free!");
+      Console.WriteLine("Pastries: $2/each, or 3 for $5");
       Console.WriteLine("----------------------------");
       Console.WriteLine("How many loaves of bread would you like?");
       int orderedBread = int.Parse(Console.ReadLine());
